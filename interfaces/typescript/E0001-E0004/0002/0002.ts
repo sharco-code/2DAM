227b5ad6296aab2cf41bf04743b2231cdfe04ced
Jose <38721575+sharco-code@users.noreply.github.com>
2019-11-12 13:52:33 +0100

    TypeScript

---
+++ interfaces/typescript/E0001-E0004/0002/0002.ts
@@ -0,0 +1,10 @@
+  var isDone: boolean = false;
+  var height: number = 6;
+  var nom: string = "bob";
+  var list:number[] = [1, 2, 3];
+  var notSure: any = 4;
+  function warnUser(): void {
+    alert("This is my warning message");
+  }
+
+  document.body.innerHTML = "Valor: "+ list[1];
