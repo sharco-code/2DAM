8d02bf0dfd1930df3aef5c1bf71225e7eb798043
Jose <38721575+sharco-code@users.noreply.github.com>
2019-11-13 12:39:23 +0100

    update 0001.ts

--- interfaces/typescript/E0001-E0004/0001/0001.ts
+++ interfaces/typescript/E0001-E0004/0001/0001.ts
@@ -2,5 +2,5 @@ function greeter(person) {
     return "Hello, " + person;
 }
 let user = "Jane User";
-document.body.innerHTML = greeter(user)+" <br> <p id='demo'></p>";
+document.body.innerHTML = greeter(user) +" <br> <p id='demo'></p>";
 document.getElementById("demo").innerHTML = "Hello World!";
