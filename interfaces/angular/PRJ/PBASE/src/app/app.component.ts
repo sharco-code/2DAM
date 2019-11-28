85e5964cd7262f9d51126da636143b351b2d9a2d
Jose <38721575+sharco-code@users.noreply.github.com>
2019-11-28 12:42:57 +0100

    angular

--- interfaces/angular/PRJ/PBASE/src/app/app.component.ts
+++ interfaces/angular/PRJ/PBASE/src/app/app.component.ts
@@ -6,19 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Jose';
+  coche = '';
+
 
   private stat = true;
 
   public id1 = "InputID1"
-  public isDisabled = true;
+  public isDisabled = false;
   public type = "text";
 
   public myStyle = "red";
+
+  public padding = "4%";
 
   public mycolors = {
     BGcolor1:this.stat,
     TXTcolor1:this.stat,
     TXTproperty1:this.stat
   };
+
+  public contador = 0;
+  public texto = "";
+
+  public click(){
+    this.contador += 10;
+  }
+  public initalD() {
+    this.contador = 20;
+    console.log("Se ha cargado el valor"+ this.contador);
+  }
+
+  public onClick(value) {
+    this.contador++;
+    this.texto = "hola";
+  }
+
+  //-----------------
+
+  poli = {
+    name:"Scooby doo papa",
+    date:"16/16/1923"
+  }
+
+  CambiaCoche(event) {
+    this.coche = event.value;
+    console.log("CambiaCoche(event)");
+  }
 }
