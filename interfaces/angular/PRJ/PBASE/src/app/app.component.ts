f2c49d9c1ebe431da4ad3b5dcd51f2134e9a9888
Jose <38721575+sharco-code@users.noreply.github.com>
2019-11-29 13:47:54 +0100

    angular

--- interfaces/angular/PRJ/PBASE/src/app/app.component.ts
+++ interfaces/angular/PRJ/PBASE/src/app/app.component.ts
@@ -25,11 +25,11 @@ export class AppComponent  {
     TXTproperty1:this.stat
   };
 
-  public contador = 0;
+  public contador = 2;
   public texto = "";
 
   public click(){
-    this.contador += 10;
+    this.contador = this.contador * this.contador;
   }
   public initalD() {
     this.contador = 20;
@@ -52,4 +52,25 @@ export class AppComponent  {
     this.coche = event.value;
     console.log("CambiaCoche(event)");
   }
+
+  movies: Movie[] = [  
+    {title:'Zootopia',director:'Byron Howard, Rich Moore',cast:'Idris Elba, Ginnifer Goodwin, Jason Bateman',releaseDate:'March 4, 2016',ciudad:'Madrid'},  
+    {title:'Batman v Superman: Dawn of Justice',director:'Zack Snyder',cast:'Ben Affleck, Henry Cavill, Amy Adams',releaseDate:'March 25, 2016', ciudad:'Madrid'},  
+    {title:'Captain America: Civil War',director:'Anthony Russo, Joe Russo',cast:'Scarlett Johansson, Elizabeth Olsen, Chris Evans',releaseDate:'May 6, 2016',ciudad:'Barcelona'},  
+    {title:'X-Men: Apocalypse',director:'Bryan Singer',cast:'Jennifer Lawrence, Olivia Munn, Oscar Isaac',releaseDate:'May 27, 2016',ciudad:'Valencia'}  
+  ]  
+  
+  valida() {
+    this.movies[0]
+  }
+
 }
+
+
+export interface Movie {  
+  title : string;  
+  director : string;  
+  cast : string;  
+  releaseDate : string;  
+  ciudad:string;
+}  
