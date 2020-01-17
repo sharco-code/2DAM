255c27a8f14c5d6222ddafdc50717a193a887a38
Jose <38721575+sharco-code@users.noreply.github.com>
2020-01-17 10:22:43 +0100

    ts

--- interfaces/angular/PRJ/pbaser/src/app/comp/deptos/productos/lista-productos/lista-productos.component.ts
+++ interfaces/angular/PRJ/pbaser/src/app/comp/deptos/productos/lista-productos/lista-productos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Iproductos } from 'src/app/models/iproductos';
+import { ProductosService } from '../services/productos.service';
 
 @Component({
   selector: 'app-lista-productos',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProductosComponent implements OnInit {
 
-  constructor() { }
+  Title="Lista de productos";
+  ImageWith=50;
+  ImageMargin=2;
+  ShowImage=false;
+  errorMessage="";
+  _listFilter="";
+
+  ProductosFiltrados:Iproductos[]=[];
+  productos:Iproductos[]=[];
+
+  constructor(private api:ProductosService) { }
 
   ngOnInit() {
+    this.api.getProductos().subscribe({
+      next: productos =>{
+        this.productos = this.productos;
+        this.ProductosFiltrados=this.productos;
+      },
+      error:err=>this.errorMessage=err});
+    }
+  toggleImage(){
+    this.ShowImage=!this.ShowImage;
   }
-
 }
+
